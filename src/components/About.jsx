39d00baab26a357455e70882f849934a7dbc1d6d
Mamang Touthang <huebart16@gmail.com>
2022-12-13 01:44:15 +0530

    details updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         About
       </h2>
       <p>
-      I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+      I am a fullstack developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
       </p>
     </div>
   )
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import profile from '../image/profile-image.png'
+// import profile from '../image/profile-image.png'
+import me from "../image/me.png"
 import mail from "../image/mail-icon.png"
 import vector from "../image/Vector-icon.png"
 
 const HeroSection = () => {
   return (
     <div className='hero-section'>
-        <img className= "profile" src={profile} alt="" />
+        <img className= "profile" src={me} alt="" />
         <div className='hero-typo'>
-            <h1> Laura Smith</h1>
-            <h3> Frontend Developer</h3>
-            <p>laurasmith.website</p>
+            <h1> M Touthang</h1>
+            <h3> Fullstack Developer</h3>
+            <p>ngtouthang.netlify.app</p>
         </div>
         
         <div className='p-button'>
